a2e2f670419bc4a73e0b5d5201104660f0f00183
San Shwe <sanxhwe@gmail.com>
2024-03-12 20:24:32 +0630

    first commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,15 @@
 import { useState } from 'react'
 import './App.css'
+import Login from './pages/login/Login'
+import SignUp from './pages/signup/SignUp'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className='p-4 h-screen flex items-center justify-center'>
-      <Login />
+      <SignUp />
     </div>
   )
 }
---
+++ frontend/src/pages/signup/Signup.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const SignUp = () => {
+  return (
+    <div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
+			<div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
+				<h1 className='text-3xl font-semibold text-center text-gray-300'>
+					Sign Up <span className='text-blue-500'> ChatApp</span>
+				</h1>
+
+        
+      </div>
+    </div>
+  )
+}
+
+export default SignUp
